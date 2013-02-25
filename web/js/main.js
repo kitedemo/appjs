fa2ac038f48d6df7eb2d07afd25c4fa29cca9948
Christine Thayer <christine.thayer@kik.com>
2013-02-25 12:11:32 -0500

    test

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,11 @@
-App.populator('Perez1', function (page, article) {
+App.populator('Perez1', function (page, articleData) {
   $(page).find('headline').clickable();
-  $(page).find('#headline').text(head[0]);
-  $(page).find('#description').text(descr[0]);
-  //$(page).find('#image').replaceWith(images[0]);
+  $(page).find('#headline').text(articleData[0].head);
+  $(page).find('#description').text(articleData[0].brief);
+
+  var $images = articleData[1].img;
+  var $newImage = $('<div class="image"></div>');
+  $(page).find('#image').replaceWith($images);
 
   // Send a Kik message
   $(page).find('#kik-it').on('click', function () {
@@ -16,8 +19,9 @@ App.populator('Perez1', function (page, article) {
 
   // Tapping headline/image goes to full article on perezhilton.com
   $(page).find('#headline').on('click', function () {
-    cards.browser.open('http://perezhilton.com/2013-02-21-taylor-swift-tom-odell-dating-sighting-london-pub-brit-awards-harry-styles-one-direction/?from=topstory_perezhilton'); 
+    cards.browser.open('http://www.google.com'); 
   });
+
   $(page).find('#myImage').on('click', function () {
     // Go to full article on perezhilton.com
     cards.browser.open('http://perezhilton.com/2013-02-21-taylor-swift-tom-odell-dating-sighting-london-pub-brit-awards-harry-styles-one-direction/?from=topstory_perezhilton'); 
@@ -26,18 +30,34 @@ App.populator('Perez1', function (page, article) {
   // Go to the "Next" page
   $(page).find('#Next').on('click', function () {
     // Go to next article
-    App.load('Perez2', article);
+    App.load('Perez1', articleData[1]);
   });
 
 });
 
-var head = ['Taylor Swift Dating Brit Award Winner Tom Odell!? See PICS Of Their First Night Together HERE!', 'h2', 'h3', 'h4', 'h5'];
-var descr =['Hold on to your hat, Harry Styles -- the man-eating Princess of Pop is hungry for a new young British boy! Taylor Swift was seen at Groucho Club in London last night with Britains rising star, Tom Odell! The two even got tipsy at an authentic English pub beforehand!', 'd2', 'd3', 'd4', 'd5'];
+var articleData = [
+  {
+    head: 'Headline1',
+    img: 'img/p1.jpg',
+    brief: 'Description for headline1',
+    link: 'www.perezhilton.com/1',
+  },
+  {
+    head: 'Headline2',
+    img: 'img/p2.jpg',
+    brief: 'Description for headline2',
+    link: 'www.perezhilton.com/2',
+  },
+  {
+    head: 'Headline3',
+    img: 'img/p3.jpg',
+    brief: 'Description for headline3',
+    link: 'www.perezhilton.com/3',
+  },
+];
 var images = ['img/p1.jpg', 'img/p2.png', 'img/p3.png'];
-var link = ['www.perezhilton.com']
-var article;
 
-App.load('Perez1', article);
+App.load('Perez1', articleData);
 
 App.populator('Perez2', function (page) {
   // put stuff here
