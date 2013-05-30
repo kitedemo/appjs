9ef274561df61580d6cf0e37e6ad3e6573e722b4
christinethayer <christine.thayer@kik.com>
2013-05-30 14:25:57 -0400

    Testing...

---
+++ web/img/noun_video_files/deckNP_js.php
@@ -0,0 +1,17 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<a href="http://www.squarespace.com/?channel=display&subchannel=thedeck&campaign=logo">';
+advert+='<img class="ad" alt="Squarespace" src="http://connect.decknetwork.net/i/squares_logo.png" border="0" height="90" width="120" />';
+advert+='</a>';
+advert+='<p class="ads">';
+advert+='<a href="http://www.squarespace.com/?channel=display&subchannel=thedeck&campaign=logo">Squarespace:</a> Everything you need to create an exceptional website.';
+advert+='</p>';
+
+
+
+
+document.write(advert);
+
+
